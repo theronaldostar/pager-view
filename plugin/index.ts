1105b0acf4bc5c9e78163fb90a3350ef67a1cf40
Ronaldo S. <ronaldo.rs44@gmail.com>
2025-02-02 03:12:05 -0300

    chore: update plugin for Vite.js projects

--- plugin/index.ts
+++ plugin/index.ts
@@ -1,1 +1,27 @@
-export * from "pager-view/plugin/vite";
+type ReactNativeProps = { [key: string]: string };
+
+const ReactNative = (alias: ReactNativeProps) => {
+	const extensions = [".css", ".json", ".web.js", ".web.ts", ".web.tsx", ".js", ".ts", ".tsx"];
+
+	return {
+		name: "vite:react-native-web",
+		config: () => ({
+			optimizeDeps: {
+				force: true,
+				esbuildOptions: {
+					resolveExtensions: extensions,
+				},
+			},
+			resolve: {
+				alias: {
+					"react-native": "react-native-web",
+					...alias,
+				},
+				extensions,
+			},
+		}),
+	};
+};
+
+export default ReactNative;
+export { ReactNative, type ReactNativeProps };
